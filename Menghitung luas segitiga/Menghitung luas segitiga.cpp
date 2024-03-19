0cbdde3536bc97dda35873eaf82990e28575debd
jaddu17 <shafajaddu@gmail.com>
2024-03-19 11:44:26 +0700

    Menghitung Luas Persegi

---
+++ Menghitung luas segitiga/Menghitung luas segitiga.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int sisi;
+
+void inputdata() {
+    cout << "Masukan Nilai Sisi :";
+    cin >> sisi;
+}
+int hitungluas() {
+    return sisi * sisi;
+}
+void display() {
+    cout << "Luas Persegi :" << hitungluas() << endl;
+}
+int main()
+{
+    inputdata();
+    display();
+}
+
