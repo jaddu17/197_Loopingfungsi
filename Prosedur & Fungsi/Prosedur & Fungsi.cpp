3ec0c50a401309442e758649aa0a525b73c23ca4
jaddu17 <shafajaddu@gmail.com>
2024-03-19 11:31:06 +0700

    Menghitung Luas Persegi

---
+++ Prosedur & Fungsi/Prosedur & Fungsi.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int panjang, lebar;
+
+void inputdata() {
+    cout << "Masukan Nilai Panjang :";
+    cin >> panjang;
+    cout << "Masukan Nilai Lebar :";
+    cin >> lebar;
+}
+int hitungluas() {
+    return panjang * lebar;
+}
+void bebas() {
+    cout << "luasnya adalah :" << hitungluas() << endl;
+}
+int main()
+{
+    inputdata();
+    bebas();
+}
+
+
